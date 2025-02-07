b560d4fa2829e047dc32106cf2dafc93240ed258
BSKBGR <santosh.padma21@yahoo.com>
2025-02-07 11:54:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -28,3 +28,18 @@ resource "aws_instance" "db_server" {
     Name = "DBServer"
   }
 }
+
+
+resource "aws_db_instance" "database" {
+  engine            = "mysql"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  db_name           = "mydb"
+  username         = "admin"
+  password         = "password"
+  skip_final_snapshot = true
+  
+  vpc_security_group_ids = [aws_security_group.sps-igw]
+  
+  subnet_group_name = aws_db_subnet_group.my_subnet_group.name
+}
